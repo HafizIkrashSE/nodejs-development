924da5b59a3c4c221aae9597eb20738c4fefcd96
HafizIkrashSE <hafizikrash809@gmail.com>
2026-09-12 15:39:42 +0500

    express.js server

---
+++ 02-express.js server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app= express();
+
+app.get("/",(req,res)=>{
+    return res.send("Hello from homepage.");
+});
+
+app.get("/about", (req,res)=>{
+    return res.send(`Hello ${req.query.name}`);
+});
+
+app.listen(3000,() => console.log("server started!"));
